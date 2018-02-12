18a97ceb9060ed0f32a9adf047988867b8d8f3af
Andy <webmaster@RealityRipple.com>
2018-02-11 20:35:33 -0800

    Rewrite for BC Math
    "BigInt" class added to wrap all GMP and BC Math functions.
    Everything but isMessageSignatureValid thrown into an "AddrTools" class.
    Support for Script addresses, TestNets, Bitcoin Gold, and Zcash added.

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # php-bitcoin-signature-routines
 
-PHP routines for verifying Bitcoin signatures.  Requires PHP 5.3.2 and the extension GMP 4.2.0 or better. GMP may be available in a package called "php5-gmp" or similar for your system.
+PHP routines for verifying Bitcoin signatures.  Requires PHP 5.3.2 with either GMP or BC Math.
 
-The main code is in verifymessage.php, with tests in test/verifymessage.php.  You will probably want to encapsulate the code more nicely, but since the ECC library needs to be configured specially and I am not up-to-date on the latest PHP packaging hotness anyway, I figured it would be best to leave that up to users to do how they like best.
+At present, Bitcoin, Bitcoin Gold, and Zcash Base56 addresses are supported. Other addresses of the same format can be added through the addrInfo function fairly easily.
+
+The isMessageSignatureValid function now returns a string with results rather than throwing errors. Possible results are:
+  * `Valid` The Address matches the Message and Signature.
+  * `Invalid` The Address does not match the Message and Signature.
+  * `Invalid Address Length` The Decoded Address is not 20 bytes + Version Length (1 for most, 2 for Zcash).
+  * `Invalid Address Checksum` The last four bytes of the Decoded Address are not equal to the first four bytes of a double-sha256 hash of the Decoded Address.
+  * `Invalid Address` The Address Version is not supported.
+  * `Invalid Signature` The Signature could not be Base64 decoded.
+  * `Invalid Signature Length` The Decoded Signature was not 65 bytes long.
+  * `Invalid Signature Flags` The Signature's Recovery Flags were either negative or greater than 7.
+  * `Invalid Public Key` The attempt to determine the Public Key from the Message and Signature failed.
--- verifymessage.php
+++ verifymessage.php
@@ -1,198 +1,214 @@
 <?php
-// PHP 5.3.2 and GMP 4.2.0 (for larger base conversions), or higher, required
+ if (extension_loaded('gmp'))
+  define('USE_EXT', 'GMP');
+ else if(extension_loaded('bcmath') && !defined('USE_EXT'))
+  define ('USE_EXT', 'BCMATH');
+ else
+  die('GMP or BC Math extensions required.');
 
-// configure the ECC lib
-if (extension_loaded('gmp')) {
-	define('USE_EXT', 'GMP');
-} else {
-	die('GMP extension required.'); // It may be available in a package called "php5-gmp" or similar for your system
-}
+ spl_autoload_register
+ (
+  function ($f)
+  {
+   $base = dirname(__FILE__)."/phpecc/";
+   $interfaceFile = $base."classes/interface/".$f."Interface.php";
+   if (file_exists($interfaceFile))
+    require_once $interfaceFile;
+   $classFile = $base."classes/".$f.".php";
+   if (file_exists($classFile))
+    require_once $classFile;
+   $utilFile = $base."classes/util/".$f.".php";
+   if (file_exists($utilFile))
+    require_once $utilFile;
+  }
+ );
 
-// Setup-stuff cribbed from index.php in the ECC repo
-spl_autoload_register(function ($f) {
-  $base = dirname(__FILE__)."/phpecc/";
-	$interfaceFile = $base . "classes/interface/" . $f . "Interface.php";
+ require_once(dirname(__FILE__).'/bigint.php');
 
-	if (file_exists($interfaceFile)) {
-		require_once $interfaceFile;
-	}
+ function isMessageSignatureValid($address, $signature, $message)
+ {
+  if (USE_EXT == 'GMP')
+  {
+   $secp256k1   = new CurveFp('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F', '0', '7');
+   $secp256k1_G = new Point($secp256k1,
+     '0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798',
+     '0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8',
+     '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141');
+  }
+  else
+  {
+   $secp256k1   = new CurveFp('115792089237316195423570985008687907853269984665640564039457584007908834671663', '0', '7');
+   $secp256k1_G = new Point($secp256k1,
+     '55066263022277343669578718895168534326250603453777594175500187360389116729240',
+     '32670510020758816978083085130507043184471273380659243275938904335757337482424',
+     '115792089237316195423570985008687907852837564279074904382605163141518161494337');
+  }
+  $address = AddrTools::base58check_decode($address);
+  if ($address === 'Checksum Mismatch')
+   return 'Invalid Address Checksum';
+  if ((strlen($address) < 21) || (strlen($address) > 22))
+   return 'Invalid Address Length';
+  list($coin, $preLen) = AddrTools::addrInfo($address);
+  if ($coin === 'Null' || $preLen === 0)
+   return 'Invalid Address';
+  $magic   = "$coin Signed Message:\n";
+  $addPre  = substr($address, 0, $preLen);
+  $signature = base64_decode($signature, true);
+  if ($signature === false)
+   return 'Invalid Signature';
+  if (strlen($signature) != 65)
+   return 'Invalid Signature Length';
+  $recoveryFlags = ord($signature[0]) - 27;
+  if ($recoveryFlags < 0 || $recoveryFlags > 7)
+   return 'Invalid Signature Flags';
+  $isCompressed = ($recoveryFlags & 4) != 0;
+  $msgMagic = AddrTools::numToVarIntString(strlen($magic)).$magic;
+  $msgData = AddrTools::numToVarIntString(strlen($message)).$message;
+  $messageHash = hash('sha256', hash('sha256', $msgMagic.$msgData, true), true);
+  $pubkey = AddrTools::recoverPubKey(BigInt::bin2big(substr($signature, 1, 32)), BigInt::bin2big(substr($signature, 33, 32)), BigInt::bin2big($messageHash), $recoveryFlags, $secp256k1_G);
+  if ($pubkey === false)
+   return 'Invalid Public Key';
+  $point = $pubkey->getPoint();
+  if (!$isCompressed)
+   $pubBinStr = "\x04".str_pad(BigInt::big2bin($point->getX()), 32, "\x00", STR_PAD_LEFT).str_pad(BigInt::big2bin($point->getY()), 32, "\x00", STR_PAD_LEFT);
+  else
+   $pubBinStr = (AddrTools::isBignumEven($point->getY()) ? "\x02" : "\x03").str_pad(BigInt::big2bin($point->getX()), 32, "\x00", STR_PAD_LEFT);
+  $derivedAddress = $addPre.hash('ripemd160', hash('sha256', $pubBinStr, true), true);
+  if ($address === $derivedAddress)
+   return 'Valid';
+  return 'Invalid';
+ }
+ 
+ class AddrTools
+ {
+  public static function addrInfo($rawAddr)
+  {
+   switch (ord($rawAddr[0]))
+   {
+    case 0:
+    case 5:
+    case 111:
+    case 196:
+     return array('Bitcoin', 1);
+     break;
+    case 23:
+    case 38:
+     return array('Bitcoin Gold', 1);
+     break;
+    case 28:
+     switch (ord($rawAddr[1]))
+     {
+      case 37:
+      case 184:
+      case 186:
+      case 189:
+       return array('Zcash', 2);
+       break;
+     }
+     break;
+   }
+   return array('Null', 0);
+  }
 
-	$classFile = $base . "classes/" . $f . ".php";
-	if (file_exists($classFile)) {
-		require_once $classFile;
-	}
+  public static function isBignumEven($bnStr)
+  {
+   return (((int)$bnStr[strlen($bnStr)-1]) & 1) == 0;
+  }
 
-	$utilFile = $base . "classes/util/" . $f . ".php";
-	if (file_exists($utilFile)) {
-		require_once $utilFile;
-	}
-});
+  public static function recoverPubKey($r, $s, $e, $recoveryFlags, $G)
+  {
+   $isYEven = ($recoveryFlags & 1) != 0;
+   $isSecondKey = ($recoveryFlags & 2) != 0;
+   $curve = $G->getCurve();
+   $signature = new Signature($r, $s);
+   static $p_over_four;
+   if (!$p_over_four)
+    $p_over_four = BigInt::div(BigInt::add($curve->getPrime(), 1), 4);
+   if (!$isSecondKey)
+    $x = $r;
+   else
+    $x = BigInt::add($r, $G->getOrder());
+   $alpha = BigInt::mod(BigInt::add(BigInt::add(BigInt::pow($x, 3), BigInt::mul($curve->getA(), $x)), $curve->getB()), $curve->getPrime());
+   $beta = NumberTheory::modular_exp($alpha, $p_over_four, $curve->getPrime());
+   if (AddrTools::isBignumEven($beta) == $isYEven)
+    $y = BigInt::sub($curve->getPrime(), $beta);
+   else
+    $y = $beta;
+   $R = new Point($curve, $x, $y, $G->getOrder());
+   $point_negate = function($p) { return new Point($p->curve, $p->x, BigInt::neg($p->y), $p->order); };
+   $rInv = NumberTheory::inverse_mod($r, $G->getOrder());
+   $eGNeg = $point_negate(Point::mul($e, $G));
+   $Q = Point::mul($rInv, Point::add(Point::mul($s, $R), $eGNeg));
+   $Qk = new PublicKey($G, $Q);
+   if ($Qk->verifies($e, $signature))
+    return $Qk;
+   return false;
+  }
 
-function isMessageSignatureValid($address, $signature, $message) {
-  // curve definition
-  // http://www.secg.org/download/aid-784/sec2-v2.pdf
-  $secp256k1 = new CurveFp(
-    '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F', '0', '7');
-  $secp256k1_G = new Point($secp256k1,
-    '0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798',
-    '0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8',
-    '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141');
+  public static function convBase($numberInput, $fromBaseInput, $toBaseInput)
+  {
+   if ($fromBaseInput == $toBaseInput)
+    return $numberInput;
+   $fromBase = str_split($fromBaseInput,1);
+   $toBase = str_split($toBaseInput,1);
+   $number = str_split($numberInput,1);
+   $fromLen=strlen($fromBaseInput);
+   $toLen=strlen($toBaseInput);
+   $numberLen=strlen($numberInput);
+   $retval='';
+   if ($toBaseInput == '0123456789')
+   {
+    $retval = 0;
+    for ($i = 1;$i <= $numberLen; $i++)
+     $retval = bcadd($retval, bcmul(array_search($number[$i-1], $fromBase),bcpow($fromLen,$numberLen-$i)));
+    return $retval;
+   }
+   if ($fromBaseInput != '0123456789')
+    $base10 = AddrTools::convBase($numberInput, $fromBaseInput, '0123456789');
+   else
+    $base10 = $numberInput;
+   if ($base10<strlen($toBaseInput))
+    return $toBase[$base10];
+   while ($base10 != '0')
+   {
+    $retval = $toBase[bcmod($base10,$toLen)].$retval;
+    $base10 = bcdiv($base10,$toLen,0);
+   }
+   return $retval;
+  }
 
-	// extract parameters
-	$address = base58check_decode($address);
-	if (strlen($address) != 21 || $address[0] != "\x0") {
-		throw new InvalidArgumentException('invalid Bitcoin address');
-	}
+  public static function base58check_decode($str)
+  {
+   $sV = ltrim(strtr($str, '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz', '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuv'), '0');
+   if (USE_EXT == 'GMP')
+    $v = gmp_init($sV, 58);
+   else
+    $v = AddrTools::convBase($sV, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuv', '0123456789');
+   $v = BigInt::big2bin($v);
+   for ($i = 0; $i < strlen($str); $i++)
+   {
+    if ($str[$i] != '1')
+     break;
+    $v = "\x00" . $v;
+   }
+   $checksum = substr($v, -4);
+   $v = substr($v, 0, -4);
+   $expCheckSum = substr(hash('sha256', hash('sha256', $v, true), true), 0, 4);
+   if ($expCheckSum != $checksum)
+    return 'Checksum Mismatch';
+   return $v;
+  }
 
-	$signature = base64_decode($signature, true);
-	if ($signature === false) {
-		throw new InvalidArgumentException('invalid base64 signature');
-	}
-
-	if (strlen($signature) != 65) {
-		throw new InvalidArgumentException('invalid signature length');
-	}
-
-	$recoveryFlags = ord($signature[0]) - 27;
-	if ($recoveryFlags < 0 || $recoveryFlags > 7) {
-		throw new InvalidArgumentException('invalid signature type');
-	}
-	$isCompressed = ($recoveryFlags & 4) != 0;
-
-	// hash message, recover key
-	$messageHash = hash('sha256', hash('sha256', "\x18Bitcoin Signed Message:\n" . numToVarIntString(strlen($message)).$message, true), true);
-	$pubkey = recoverPubKey(bin2gmp(substr($signature, 1, 32)), bin2gmp(substr($signature, 33, 32)), bin2gmp($messageHash), $recoveryFlags, $secp256k1_G);
-	if ($pubkey === false) {
-		throw new InvalidArgumentException('unable to recover key');
-	}
-	$point = $pubkey->getPoint();
-
-	// see that the key we recovered is for the address given
-	if (!$isCompressed) {
-		$pubBinStr = "\x04" . str_pad(gmp2bin($point->getX()), 32, "\x00", STR_PAD_LEFT) .
-							  str_pad(gmp2bin($point->getY()), 32, "\x00", STR_PAD_LEFT);
-	} else {
-		$pubBinStr =	(isBignumEven($point->getY()) ? "\x02" : "\x03") .
-							  str_pad(gmp2bin($point->getX()), 32, "\x00", STR_PAD_LEFT);
-	}
-	$derivedAddress = "\x00". hash('ripemd160', hash('sha256', $pubBinStr, true), true);
-
-	return $address === $derivedAddress;
-}
-
-function isBignumEven($bnStr) {
-	return (((int)$bnStr[strlen($bnStr)-1]) & 1) == 0;
-}
-
-// based on bitcoinjs-lib's implementation
-// and SEC 1: Elliptic Curve Cryptography, section 4.1.6, "Public Key Recovery Operation".
-// http://www.secg.org/download/aid-780/sec1-v2.pdf
-function recoverPubKey($r, $s, $e, $recoveryFlags, $G) {
-	$isYEven = ($recoveryFlags & 1) != 0;
-	$isSecondKey = ($recoveryFlags & 2) != 0;
-	$curve = $G->getCurve();
-	$signature = new Signature($r, $s);
-
-	// Precalculate (p + 1) / 4 where p is the field order
-	static $p_over_four; // XXX just assuming only one curve/prime will be used
-	if (!$p_over_four) {
-		$p_over_four = gmp_div(gmp_add($curve->getPrime(), 1), 4);
-	}
-
-	// 1.1 Compute x
-	if (!$isSecondKey) {
-		$x = $r;
-	} else {
-		$x = gmp_add($r, $G->getOrder());
-	}
-
-	// 1.3 Convert x to point
-	$alpha = gmp_mod(gmp_add(gmp_add(gmp_pow($x, 3), gmp_mul($curve->getA(), $x)), $curve->getB()), $curve->getPrime());
-	$beta = NumberTheory::modular_exp($alpha, $p_over_four, $curve->getPrime());
-
-	// If beta is even, but y isn't or vice versa, then convert it,
-	// otherwise we're done and y == beta.
-	if (isBignumEven($beta) == $isYEven) {
-		$y = gmp_sub($curve->getPrime(), $beta);
-	} else {
-		$y = $beta;
-	}
-
-	// 1.4 Check that nR is at infinity (implicitly done in construtor)
-	$R = new Point($curve, $x, $y, $G->getOrder());
-
-	$point_negate = function($p) { return new Point($p->curve, $p->x, gmp_neg($p->y), $p->order); };
-
-	// 1.6.1 Compute a candidate public key Q = r^-1 (sR - eG)
-	$rInv = NumberTheory::inverse_mod($r, $G->getOrder());
-	$eGNeg = $point_negate(Point::mul($e, $G));
-	$Q = Point::mul($rInv, Point::add(Point::mul($s, $R), $eGNeg));
-
-	// 1.6.2 Test Q as a public key
-	$Qk = new PublicKey($G, $Q);
-	if ($Qk->verifies($e, $signature)) {
-		return $Qk;
-	}
-
-	return false;
-}
-
-function base58check_decode($str) {
-	// strtr thanks to https://github.com/prusnak/addrgen/blob/master/php/addrgen.php
-	// ltrim because leading zeroes can mess up the parsing even if you specify the base..
-	$v = gmp_init(ltrim(strtr($str,
-		'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz',
-		'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuv'), '0'), 58);
-	$v = gmp2bin($v);
-	// for each leading 1, pre-pad the byte array with a 0
-	for ($i = 0; $i < strlen($str); $i++) {
-		if ($str[$i] != '1') {
-			break;
-		}
-		$v = "\x00" . $v;
-	}
-
-	$checksum = substr($v, -4);
-	$v = substr($v, 0, -4);
-
-	$expCheckSum = substr(hash('sha256', hash('sha256', $v, true), true), 0, 4);
-
-	if ($expCheckSum != $checksum) {
-		throw new InvalidArgumentException('invalid checksum');
-	}
-
-	return $v;
-}
-
-function numToVarIntString($i) {
-	if ($i < 0xfd) {
-		return chr($i);
-	} else if ($i <= 0xffff) {
-		return pack('Cv', 0xfd, $i);
-	} else if ($i <= 0xffffffff) {
-		return pack('CV', 0xfe, $i);
-	} else {
-		throw new InvalidArgumentException('int too large');
-	}
-}
-
-function bin2gmp($binStr) {
-	$v = gmp_init('0');
-
-	for ($i = 0; $i < strlen($binStr); $i++) {
-		$v = gmp_add(gmp_mul($v, 256), ord($binStr[$i]));
-	}
-
-	return $v;
-}
-
-function gmp2bin($v) {
-	$binStr = '';
-
-	while (gmp_cmp($v, 0) > 0) {
-		list($v, $r) = gmp_div_qr($v, 256);
-		$binStr = chr(gmp_intval($r)) . $binStr;
-	}
-
-	return $binStr;
-}
+  public static function numToVarIntString($i)
+  {
+   if ($i < 0)
+    return chr(0);
+   if ($i < 0xfd)
+    return chr($i);
+   if ($i <= 0xffff)
+    return pack('Cv', 0xfd, $i);
+   if ($i <= 0xffffffff)
+    return pack('CV', 0xfe, $i);
+   return pack('CV', 0xfe, 0xffffffff);
+  }
+ }
